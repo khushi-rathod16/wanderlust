aa42b3f4a0f8cd3e4b61e14f1abe4d4339251c29
Khushi Rathod <khushirathod.ar1712@gmail.com>
2026-06-29 17:35:18 +0530

    Fix connect-mongo import

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
 const session = require("express-session");
-const { MongoStore } = require("connect-mongo");
+const MongoStore = require("connect-mongo").default;
 const flash = require("connect-flash");
 
 const passport = require("passport");
@@ -58,7 +58,7 @@ app.use(express.static(path.join(__dirname, "/public")));
 const store = MongoStore.create({
     mongoUrl: dburl,
     crypto: {
-        secret:  "mysupersecretcode",
+        secret:  process.env.SECRET,
     },
     touchAfter: 24 * 3600,
 });
@@ -70,7 +70,7 @@ store.on("error", (err)=>{
 
 const sessionOptions = {
     store,
-    secret: "mysupersecretcode",
+    secret:  process.env.SECRET,
     resave: false,
     saveUninitialized: true,
     cookie:{
